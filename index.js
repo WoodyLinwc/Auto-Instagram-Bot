1752db6c4cec6ecf2260be0a39cf6550610a6086
Woody Lin <wancheng.lin001@umb.edu>
2023-06-19 19:22:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,45 @@ const { IgApiClient } = require('instagram-private-api');
 const { get } = require('request-promise');
 const CronJob = require("cron").CronJob;
 
+// const postToInsta = async () => {
+//     const ig = new IgApiClient();
+//     ig.state.generateDevice(process.env.IG_USERNAME);
+//     await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+
+//     const imageBuffer = await get({
+//         url: 'https://i.imgur.com/BZBHsauh.jpg',
+//         encoding: null, 
+//     });
+
+//     await ig.publish.photo({
+//         file: imageBuffer,
+//         caption: 'Really nice photo from the internet!',
+//     });
+// }
+const fs2 = require('fs');
 const postToInsta = async () => {
-    const ig = new IgApiClient();
-    ig.state.generateDevice(process.env.IG_USERNAME);
-    await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+  const ig = new IgApiClient();
+  ig.state.generateDevice(process.env.IG_USERNAME);
+  await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
 
-    const imageBuffer = await get({
-        url: 'https://i.imgur.com/BZBHsauh.jpg',
-        encoding: null, 
-    });
+  // Read the uris.json file and parse its content
+  const uris = JSON.parse(fs2.readFileSync('uris.json', 'utf8'));
 
-    await ig.publish.photo({
-        file: imageBuffer,
-        caption: 'Really nice photo from the internet!',
-    });
-}
+  // Randomly select a link from the uris array
+  const randomIndex = Math.floor(Math.random() * uris.length);
+  const randomUri = uris[randomIndex];
+
+  const imageBuffer = await get({
+      url: randomUri,
+      encoding: null, 
+  });
+
+  await ig.publish.photo({
+      file: imageBuffer,
+      caption: 'Really nice photo from the internet!',
+  });
+};
+
 
 postToInsta();
 
