1a4047c698b232c6635ae58fef074cdac2c246a3
Woody Lin <wancheng.lin001@umb.edu>
2023-06-22 13:52:50 -0400

    environment setup

--- index.js
+++ index.js
@@ -4,44 +4,30 @@ const { IgApiClient } = require('instagram-private-api');
 const { get } = require('request-promise');
 const CronJob = require("cron").CronJob;
 const fs = require("fs");
+const Jimp = require("jimp");
 
-// const postToInsta = async () => {
-//     const ig = new IgApiClient();
-//     ig.state.generateDevice(process.env.IG_USERNAME);
-//     await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-
-//     const imageBuffer = await get({
-//         url: 'https://i.imgur.com/BZBHsauh.jpg',
-//         encoding: null, 
-//     });
-
-//     await ig.publish.photo({
-//         file: imageBuffer,
-//         caption: 'Really nice photo from the internet!',
-//     });
-// }
 const postToInsta = async () => {
   const ig = new IgApiClient();
-  const sharp = require('sharp');
   ig.state.generateDevice(process.env.IG_USERNAME);
   await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-  
-  // read the uris.json file and randomly choose a image
+
+  // read the uris.json file and randomly choose an image
   const uris = JSON.parse(fs.readFileSync("uris.json", "utf8"));
   const randomIndex = Math.floor(Math.random() * uris.length);
   const uri = uris[randomIndex];
 
   const imageBuffer = await get({
-      url: uri,
-      encoding: null, 
+    url: uri,
+    encoding: null,
   });
 
-  // convert the image to acceptable format
-  const jpegBuffer = await sharp(imageBuffer).jpeg().toBuffer();
+  // convert the image to an acceptable format using Jimp
+  const jimpImage = await Jimp.read(imageBuffer);
+  const jpegBuffer = await jimpImage.getBufferAsync(Jimp.MIME_JPEG);
 
   await ig.publish.photo({
-      file: jpegBuffer,
-      caption: "#GIDLE #여자아이들",
+    file: jpegBuffer,
+    caption: "#GIDLE #여자아이들",
   });
 };
 
